2ee7b8645183cf5bdd0c3d648ea7509db975f267
kanjiro-f <kanjiro.fujimoto@leverages.jp>
2016-10-01 17:07:58 +0900

    吹き出し追加 #3

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 $(function() {
-    console.log('hello world');
+    var style = document.createElement('link');
+    style.rel = 'stylesheet';
+    style.type = 'text/css';
+    style.href = chrome.extension.getURL('fukidashi.css');
+    (document.head||document.documentElement).appendChild(style);
+    
+    $(".cassetteitem_object-item").each(function(){
+        $(this).css('position','relative');
+        $(this).append(
+            '<div class="p-fukidashiWrapper"><div class="p-fukidashi"><p class="p-fukidashi__addicted">二郎依存度</p><p class="p-fukidashi__rate">98%</p><p class="p-fukidashi__walk">徒歩<span class="p-fukidashi__min">5分</span></p></div></div>'
+        );
+    });
 });
