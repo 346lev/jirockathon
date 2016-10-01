24df009b2bae7f6a85900ce7e6406d73cf4b2432
Sanshiro Sakai <xosya3@gmail.com>
2016-10-01 18:31:58 +0900

    二郎度の計算とその表示まで

--- getgeo.js
+++ getgeo.js
@@ -1,22 +1,18 @@
-$(function () {
+function getgeo(address) {
+    var BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
+    var ACCESS_URL = BASE_URL + '?address=' + address + '&sensor=false';
 
-$.fn.getgeo = function(address) {
-var BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
-// var ACCESS_URL = BASE_URL + address;
-var ACCESS_URL = BASE_URL + '?address=' + address + '&sensor=false';
-
-var request = $.ajax({
-    type : 'GET',
-    url : ACCESS_URL,
-    success : function(data) {
-        geolocation = data.results[0].geometry.location;
-        return geolocation;
-    },
-    error : function(data) {
+    return $.ajax({
+        ContentType: 'Application/json',
+        type: 'GET',
+        async: false,
+        url: ACCESS_URL,
+    }).done(function (data) {
+        if(data.status == 'ok') {
+            return data.results[0].geometry.location;
+        }
+    }).fail(function (data) {
         console.log('error');
-    }
-});
-
+    });
 }
-});
 
